04d26922232fc72f9dd9d380d7bfcd10daad4440
Allen White <allenwhite2@dswinc.com>
2019-07-09 22:21:10 -0400

    Add transitions page to example.

--- src/routes/transitions.svelte
+++ src/routes/transitions.svelte
@@ -1,10 +1,16 @@
 <script>
-    import { fly, fade } from 'svelte/transition';
-
+    import { fly, fade, draw } from 'svelte/transition';
+    export const transitions = { draw };
     let visible = true;
 </script>
 
 <style>
+    .bottom {
+        position: absolute;
+        left: 50%;
+        top: 250%;
+        transform: translate(-50%, -50%);
+    }
     .centered {
         position: absolute;
         left: 50%;
@@ -17,15 +23,38 @@
         transform: translate(-50%, -50%);
         font-size: 4em;
     }
+
+    svg {
+		width: 100%;
+        position: absolute;
+        left: 25%;
+        top: 300%;
+	}
+	
+	path {
+		fill: none;
+		stroke: rgb(111,11,111);
+		stroke-width: 2px;
+	}
 </style>
 
 <label>
-    <input type='checkbox' bind:checked={ visible }>
-    visible
+    <input type='checkbox' bind:checked={ visible }> visible
 </label>
 
 { #if visible }
-    <div class="centered" transition:fly={{y: 250}}>
-        <span>transitions!</span>
+    <div class="bottom" transition:fly={{y: 250}}>
+        <span>!FLY!</span>
     </div>
+
+    <div class="centered" transition:fade>
+        <span>_F_A_D_E_</span>
+    </div>
+
 {/if}
+
+<svg viewBox='0 0 100 100'>
+  {#if visible}
+    <path d="M10,10 L40,10 40,20 10,20 Z" transition:draw/>
+  {/if}
+</svg>
